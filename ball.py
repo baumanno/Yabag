3de0c49d6778e5926c3bc26930ecbc52e2efacca
Oliver N. Baumann <oliver.n.baumann@gmail.com>
2012-03-08 14:09:08 +0100

    Added difficulty
    Modified ball.py to include multiple balls and detect collisions with mouse.

--- ball.py
+++ ball.py
@@ -1,39 +1,58 @@
 from pygame import * # Use pygame
+import random
 
 ballpic = image.load('ball.png')
+ballpic.set_colorkey((0,0,0))
+
+numBalls = 5
+delay = 5
 
 done = False
 
-ballX = 0
-ballY = 0 # Ball position
+balls = []
 
-ballXMove = 1
-ballYMove = 1
+for count in range(numBalls):
+    balls.append(dict)
+    balls[count] = {'x': 0,
+                    'y': 0,
+                    'xmove': random.randint(1, 2),
+                    'ymove': random.randint(1, 2)}
 
 init() # Start pygame
 screen = display.set_mode((640, 480)) # Set Window
 display.set_caption('YaBaG - Yet Another Ball Game') # Set title
 
+event.set_grab(1)
+
 while done == False:
     screen.fill(0) # Fill screen with black (0)
-    screen.blit(ballpic, (ballX, ballY)) # Draw ball
+    
+    for count in range(numBalls):
+        screen.blit(ballpic, (balls[count]['x'], balls[count]['y'])) # Draw ball
+
     display.update()
 
-    time.delay(5) # Slow down!
+    time.delay(delay) # Slow down!
 
-    ballX = ballX + ballXMove
-    ballY = ballY + ballYMove # Update ball position
+    for count in range(numBalls):
+        balls[count]['x'] = balls[count]['x'] + balls[count]['xmove']
+        balls[count]['y'] = balls[count]['y'] + balls[count]['ymove']
 
-    if ballX > 600: # Ball reached screen edges
-        ballXMove = -1
-    if ballX < 0:
-        ballXMove = 1
-    if ballY > 440:
-        ballYMove = -1
-    if ballY < 0:
-        ballYMove = 1
+        if balls[count]['x'] > 620: # Ball reached screen edges
+            balls[count]['xmove'] = random.randint(-2, 0)
+        if balls[count]['x'] < -10: 
+            balls[count]['xmove'] = random.randint(0, 2)
+        if balls[count]['y'] > 470:
+            balls[count]['ymove'] = random.randint(-2, 0)
+        if balls[count]['y'] < -10:
+            balls[count]['ymove'] = random.randint(0, 2)
 
     for e in event.get(): # Check for ESC
         if e.type == KEYUP:
             if e.key == K_ESCAPE:
                 done = True
+
+    if screen.get_at((mouse.get_pos())) == (255, 255, 255, 255):
+        done = True
+
+print "You lasted for ", time.get_ticks()/1000, " seconds!"
